faddcbb8396c20857b95f1807772dfb653d34b18
N-K-22 <khamarnitya@gmail.com>
2026-01-06 17:01:13 -0600

    iteratively developing and testing the sensor for line tracking

---
+++ src/Motor/motor.py
@@ -0,0 +1,15 @@
+import Motor
+
+
+class MotorClass:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+    def reverse(self):
+        pass
+    def right(self):
+        pass
+    def left(self):
+        pass
